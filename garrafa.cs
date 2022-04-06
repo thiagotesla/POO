e7a58dacbdd9799001b603e6a29151b7d0f1d220
Thiago Tesla <thiagobarros95@gmail.com>
2022-04-06 10:56:48 -0300

    cria classe garrafa em c#

---
+++ garrafa.cs
@@ -0,0 +1,9 @@
+public class Garrafa
+{
+    public int Altura { get; set; }
+    public int Largura { get; set; }
+    public int Capacidade { get; set; }
+
+    public void Abrir() { }
+    public void Fechar() { }
+}
