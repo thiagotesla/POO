5b6e66b39fcf6cb3f0d10e5427081a81fa8d1312
Thiago Tesla <thiagobarros95@gmail.com>
2022-04-06 10:44:59 -0300

    cria classe garrafa em javascript

---
+++ garrafa.js
@@ -0,0 +1,10 @@
+class Garrafa{
+    constructor(altura, largura, capacidade ){
+        this.altura = altura
+        this.largura = largura
+        this.capacidade = capacidade
+    }
+
+    abrir() {}
+    fechar() {}
+}
