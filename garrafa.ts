52d05085680ea93f31b16947248b840e2139ba60
Thiago Tesla <thiagobarros95@gmail.com>
2022-04-06 10:51:03 -0300

    cria classe garrafa em typescript

---
+++ garrafa.ts
@@ -0,0 +1,18 @@
+class Garrafa {
+    altura: number;
+    largura: number;
+    capacidade: number;
+
+    constructor(
+        altura: number,
+        largura: number,
+        capacidade: number,
+    ) {
+        this.altura = altura;
+        this.largura = largura;
+        this.capacidade = capacidade;
+    }
+
+    abrir() { }
+    fechar() { }
+}
